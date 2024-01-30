ca850d017e2abbdbdd106aeb349a8d067dae763f
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2024-01-30 23:45:51 +0530

    Improve search bar

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,6 +6,7 @@ import { YOUTUBE_SEARCH_API } from "../utils/constants";
 const Head = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
   // console.log(searchQuery);
 
   useEffect(() => {
@@ -48,17 +49,19 @@ const Head = () => {
       <div className="col-span-10 px-10 ml-44">
         <div>
           <input
+          className="w-1/2 px-5 border border-gray-400 p-2 rounded-l-full"
             type="text"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
-            className="w-1/2 px-5 border border-gray-400 p-2 rounded-l-full"
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
           ></input>
           <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
             🔍
           </button>
         </div>
 
-        <div className="fixed bg-white py-2 px-2 w-[471px] shadow-lg rounded-lg border border-gray-100">
+        {showSuggestions && <div className="fixed bg-white py-2 px-2 w-[471px] shadow-lg rounded-lg border border-gray-100">
           <ul>
             {suggestions.map((s) => (
               <li key={s} className="py-2 px-3 hover:bg-gray-200">
@@ -66,7 +69,7 @@ const Head = () => {
               </li>
             ))}
           </ul>
-        </div>
+        </div>}
       </div>
       <div className="col-span-1">
         <img
