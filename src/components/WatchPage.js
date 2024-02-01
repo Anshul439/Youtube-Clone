d6262537062a61e1cfcaab039680df58be285a23
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2024-02-01 22:59:32 +0530

    Add mock comments section

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import { CommentsContainer } from "./CommentsContainer";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -11,16 +12,23 @@ const WatchPage = () => {
     dispatch(closeMenu());
   }, []);
   return (
+    <div className="flex flex-col">
     <div className="px-16 py-7">
       <iframe
         width="950"
         height="500"
-        src={"https://www.youtube.com/embed/"+ searchParams.get("v") + "?autoplay=1"}
+        src={
+          "https://www.youtube.com/embed/" +
+          searchParams.get("v") +
+          "?autoplay=1"
+        }
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
+    </div>
+    <CommentsContainer />
     </div>
   );
 };
